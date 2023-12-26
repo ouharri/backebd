0317d0c3987880489c449ef907a51522f44c7a2e
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:35:04 +0100

    Repository interface for accessing and managing User entities in the database.

---
+++ backend/src/main/java/com/ouharri/Jobinow/repositories/UserRepository.java
@@ -0,0 +1,33 @@
+package com.ouharri.Jobinow.repositories;
+
+import com.ouharri.Jobinow.model.entities.User;
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Repository interface for accessing and managing {@link User} entities in the database.
+ */
+@Repository
+@NonNullApi
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    /**
+     * Finds a user by their email address.
+     *
+     * @param email The email address of the user.
+     * @return An optional containing the user, or empty if not found.
+     */
+    Optional<User> findByEmail(String email);
+
+    /**
+     * Finds a user by their ID.
+     *
+     * @param id The ID of the user.
+     * @return An optional containing the user, or empty if not found.
+     */
+    Optional<User> findById(UUID id);
+}
