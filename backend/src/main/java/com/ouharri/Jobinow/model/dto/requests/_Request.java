b3d6fc8ef1c51ef08cecc23b66f9003fa4b80ce4
ouharri <ouharrioutman@gmail.com>
2023-12-26 13:33:11 +0100

    Marker interface for request data transfer objects (DTOs). Implement this interface in all your request DTO classes. This interface serves as a common type for all request DTOs.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/dto/requests/_Request.java
@@ -0,0 +1,14 @@
+package com.ouharri.Jobinow.model.dto.requests;
+
+import java.io.Serializable;
+
+/**
+ * Marker interface for request data transfer objects (DTOs).
+ * Implement this interface in all your request DTO classes.
+ * This interface serves as a common type for all request DTOs.
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ */
+public interface _Request extends Serializable {
+    // This interface is intentionally left blank.
+    // It serves as a marker interface for request DTOs.
+}
