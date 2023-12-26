c8a6b99d248cc42d5b6b85ef5ce2bb6d093c3e24
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:27:08 +0100

    Service interface for managing User entities.

---
+++ backend/src/main/java/com/ouharri/Jobinow/services/spec/UserService.java
@@ -0,0 +1,38 @@
+package com.ouharri.Jobinow.services.spec;
+
+import com.ouharri.Jobinow.model.dto.requests.ChangePasswordRequest;
+import com.ouharri.Jobinow.model.entities.User;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Service interface for managing User entities.
+ */
+public interface UserService {
+
+    /**
+     * Retrieves a user by their unique identifier.
+     *
+     * @param id The unique identifier of the user.
+     * @return An Optional containing the user, or an empty Optional if not found.
+     */
+    Optional<User> findById(UUID id);
+
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return A list of all users.
+     */
+    List<User> getAllUsers();
+
+    /**
+     * Changes the password for the user identified by the connected user principal.
+     *
+     * @param request       The request containing the old and new passwords.
+     * @param connectedUser The principal representing the connected user.
+     */
+    void changePassword(ChangePasswordRequest request, Principal connectedUser);
+}
