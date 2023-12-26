e2b8c2255cc7493edd8d566708a9f89df6410c2c
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:24:10 +0100

    DTO class representing a request to change the user's password. Contains fields for the current password, new password, and confirmation password.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/dto/requests/ChangePasswordRequest.java
@@ -0,0 +1,25 @@
+package com.ouharri.Jobinow.model.dto.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO class representing a request to change the user's password.
+ * Contains fields for the current password, new password, and confirmation password.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ * @version 1.0
+ */
+public record ChangePasswordRequest(
+        @NotBlank(message = "Current password is required")
+        String currentPassword,
+
+        @NotBlank(message = "New password is required")
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+        String newPassword,
+
+        @NotBlank(message = "Password confirmation is required")
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+        String confirmationPassword
+) implements _Request {
+}
