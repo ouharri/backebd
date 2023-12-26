6e500d742a85babd765f4920478673acaa5ee747
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:29:41 +0100

    Custom validator for ensuring that the end time of a competition is after the start time

---
+++ backend/src/main/java/com/ouharri/Jobinow/validations/validator/EndTimeAfterStartTimeValidator.java
@@ -0,0 +1,37 @@
+package com.ouharri.Jobinow.validations.validator;
+
+import com.ouharri.Jobinow.validations.Annotation.EndTimeAfterStartTime;
+import com.ouharri.Jobinow.validations.interfaces.DateTimeValidator;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.sql.Date;
+
+/**
+ * Custom validator for ensuring that the end time of a competition is after the start time.
+ */
+public class EndTimeAfterStartTimeValidator implements ConstraintValidator<EndTimeAfterStartTime, DateTimeValidator> {
+
+    /**
+     * Validates that the end time is after the start time for a given competition.
+     *
+     * @param competition The competition to be validated.
+     * @param context     The validation context.
+     * @return true if the end time is after the start time, false otherwise.
+     */
+    @Override
+    public boolean isValid(DateTimeValidator competition, ConstraintValidatorContext context) {
+        if (competition == null) {
+            return true;
+        }
+
+        Date date = competition.getDate();
+        Date currentDate = new Date(System.currentTimeMillis());
+
+        return (date.toLocalDate().isEqual(currentDate.toLocalDate())
+                || date.toLocalDate().isAfter(currentDate.toLocalDate())) &&
+                competition.getStartTime() != null &&
+                competition.getEndTime() != null &&
+                competition.getEndTime().after(competition.getStartTime());
+    }
+}
