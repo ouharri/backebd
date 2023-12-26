2d0cf82fcc61b1cd997b08fde8f753a68113b15a
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:23:51 +0100

    Data Transfer Object (DTO) for authentication requests. This class represents the data sent when a user is trying to authenticate. It includes the user's email and password.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/dto/requests/AuthenticationRequest.java
@@ -0,0 +1,23 @@
+package com.ouharri.Jobinow.model.dto.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Data Transfer Object (DTO) for authentication requests.
+ * This class represents the data sent when a user is trying to authenticate.
+ * It includes the user's email and password.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">Ouharri Outman</a>
+ */
+public record AuthenticationRequest(
+        @Email(message = "Email should be valid")
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+        String password
+) implements _Request {
+}
