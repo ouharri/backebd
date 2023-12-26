b2c5f6f538333970cb00ac62f6867c8040f05e21
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:21:32 +0100

    Abstract base class for entities in the Aftas application. Provides common fields such as id, creation timestamp, and update timestamp. Subclasses should use the @MappedSuperclass annotation.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/entities/AbstractEntity.java
@@ -0,0 +1,68 @@
+package com.ouharri.Jobinow.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Abstract base class for entities in the Aftas application.
+ * Provides common fields such as id, creation timestamp, and update timestamp.
+ * Subclasses should use the @MappedSuperclass annotation.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ * @version 1.0
+ */
+@Getter
+@Setter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractEntity implements _Entity<UUID> {
+
+    /**
+     * The unique identifier for the entity.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    /**
+     * The timestamp indicating when the entity was created.
+     */
+    @CreationTimestamp
+    @ReadOnlyProperty
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Timestamp createdAt;
+
+    /**
+     * The timestamp indicating when the entity was last updated.
+     */
+    @UpdateTimestamp
+    @ReadOnlyProperty
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp updatedAt;
+
+    /**
+     * The version of the entity, used for optimistic locking.
+     */
+    @Version
+    @ReadOnlyProperty
+    private Long version = 0L;
+}
