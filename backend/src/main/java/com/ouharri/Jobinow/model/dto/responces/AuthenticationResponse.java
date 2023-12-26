6a23685311d2a400d24f0f7c82798752415f3952
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:22:17 +0100

    DTO class representing the response for authentication. Contains access and refresh tokens.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/dto/responces/AuthenticationResponse.java
@@ -0,0 +1,35 @@
+package com.ouharri.Jobinow.model.dto.responces;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO class representing the response for authentication.
+ * Contains access and refresh tokens.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ * @version 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse implements _Response {
+    /**
+     * Access token for authentication.
+     */
+    @NotBlank(message = "Access token is required")
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    /**
+     * Refresh token for token refreshing.
+     */
+    @NotBlank(message = "Refresh token is required")
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
