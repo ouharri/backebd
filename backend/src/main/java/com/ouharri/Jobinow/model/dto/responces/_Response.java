e319fb6c0ca2811db6fb3472c13a2283417754d3
ouharri <ouharrioutman@gmail.com>
2023-12-26 13:52:48 +0100

    Marker interface for response data transfer objects (DTOs). Implement this interface in all your response DTO classes. This interface serves as a common type for all response DTOs.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/dto/responces/_Response.java
@@ -0,0 +1,14 @@
+package com.ouharri.Jobinow.model.dto.responces;
+
+import java.io.Serializable;
+
+/**
+ * Marker interface for response data transfer objects (DTOs).
+ * Implement this interface in all your response DTO classes.
+ * This interface serves as a common type for all response DTOs.
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ */
+public interface _Response extends Serializable {
+    // This interface is intentionally left blank.
+    // It serves as a marker interface for response DTOs.
+}
