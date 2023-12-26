91288add17a80225df5e6d50b7a5233df7b84d80
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:28:27 +0100

    Validator for the FutureDate constraint. Ensures that the provided date is in the future.

---
+++ backend/src/main/java/com/ouharri/Jobinow/validations/validator/FutureDateValidator.java
@@ -0,0 +1,33 @@
+package com.ouharri.Jobinow.validations.validator;
+
+import com.ouharri.Jobinow.validations.Annotation.FutureDate;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.sql.Date;
+
+/**
+ * Validator for the {@link FutureDate} constraint.
+ * Ensures that the provided date is in the future.
+ */
+public class FutureDateValidator implements ConstraintValidator<FutureDate, Date> {
+
+    /**
+     * Validates whether the provided date is in the future.
+     *
+     * @param date    The date to be validated.
+     * @param context The validation context.
+     * @return {@code true} if the date is in the future or {@code null}, {@code false} otherwise.
+     */
+    @Override
+    public boolean isValid(Date date, ConstraintValidatorContext context) {
+        if (date == null) {
+            return true;
+        }
+
+        Date currentDate = new Date(System.currentTimeMillis());
+
+        return date.toLocalDate().isEqual(currentDate.toLocalDate())
+                || date.toLocalDate().isAfter(currentDate.toLocalDate());
+    }
+}
