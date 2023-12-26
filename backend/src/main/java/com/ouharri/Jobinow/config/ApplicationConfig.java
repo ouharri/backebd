88f1ab22ef40e038f5ad72bf61ec1af4d60dd476
ouharri <ouharrioutman@gmail.com>
2023-12-26 14:26:16 +0100

    Configuration class for application-related beans and settings.

---
+++ backend/src/main/java/com/ouharri/Jobinow/config/ApplicationConfig.java
@@ -0,0 +1,97 @@
+package com.ouharri.Jobinow.config;
+
+import com.ouharri.Jobinow.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+/**
+ * Configuration class for application-related beans and settings.
+ *  @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ */
+@Configuration
+@RequiredArgsConstructor
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    /**
+     * Creates a custom implementation of UserDetailsService to load user details by email.
+     *
+     * @return UserDetailsService implementation
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    /**
+     * Configures and provides a custom AuthenticationProvider using DaoAuthenticationProvider.
+     *
+     * @return AuthenticationProvider
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    /**
+     * Creates an instance of AuditorAware to provide the current user's ID for entity auditing.
+     *
+     * @return AuditorAware implementation
+     */
+    @Bean
+    public AuditorAware<UUID> auditorAware() {
+        return new ApplicationAuditAware();
+    }
+
+    /**
+     * Retrieves the AuthenticationManager from the provided AuthenticationConfiguration.
+     *
+     * @param config AuthenticationConfiguration
+     * @return AuthenticationManager
+     * @throws Exception if an error occurs while retrieving the AuthenticationManager
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    /**
+     * Creates an instance of BCryptPasswordEncoder as the password encoder.
+     *
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Creates an instance of ModelMapper to map entities to DTOs.
+     *
+     * @return ModelMapper
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
