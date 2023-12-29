61842fd471675b45d53403243235418e2711170a
ouharri <ouharrioutman@gmail.com>
2023-12-29 15:59:14 +0100

    Service implementation for sending emails using Spring's JavaMailSender.

---
+++ backend/src/main/java/com/ouharri/Jobinow/services/impl/EmailServiceImpl.java
@@ -0,0 +1,65 @@
+package com.ouharri.Jobinow.services.impl;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+/**
+ * Service implementation for sending emails using Spring's JavaMailSender.
+ */
+@Service
+@AllArgsConstructor
+public class EmailServiceImpl {
+
+    private final JavaMailSender emailSender;
+
+    private static final String FROM_ADDRESS = "noreply@baeldung.com";
+
+    /**
+     * Sends a simple email message.
+     *
+     * @param to      The recipient's email address.
+     * @param subject The subject of the email.
+     * @param text    The content of the email.
+     */
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(FROM_ADDRESS);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    /**
+     * Sends an email message with an attachment.
+     *
+     * @param to               The recipient's email address.
+     * @param subject          The subject of the email.
+     * @param text             The content of the email.
+     * @param pathToAttachment The file path to the attachment.
+     * @throws MessagingException Thrown if there is an issue with creating or sending the email message.
+     */
+    public void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(FROM_ADDRESS);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+
+        FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+        helper.addAttachment("Invoice", file);
+
+        emailSender.send(message);
+    }
+}
