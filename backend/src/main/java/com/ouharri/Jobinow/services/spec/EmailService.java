1ea6c5e94522308682c3d395576be69c7a7d8130
ouharri <ouharrioutman@gmail.com>
2023-12-29 15:55:44 +0100

    Interface defining email-related services.

---
+++ backend/src/main/java/com/ouharri/Jobinow/services/spec/EmailService.java
@@ -0,0 +1,29 @@
+package com.ouharri.Jobinow.services.spec;
+
+import jakarta.mail.MessagingException;
+
+/**
+ * Interface defining email-related services.
+ */
+public interface EmailService {
+
+    /**
+     * Sends a simple email message.
+     *
+     * @param to      The recipient's email address.
+     * @param subject The subject of the email.
+     * @param text    The content of the email.
+     */
+    void sendSimpleMessage(String to, String subject, String text);
+
+    /**
+     * Sends an email message with an attachment.
+     *
+     * @param to               The recipient's email address.
+     * @param subject          The subject of the email.
+     * @param text             The content of the email.
+     * @param pathToAttachment The file path to the attachment.
+     * @throws MessagingException Thrown if there is an issue with creating or sending the email message.
+     */
+    void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException;
+}
