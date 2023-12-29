1ad521e68f919f918de6722d4228041ed31519f2
ouharri <ouharrioutman@gmail.com>
2023-12-29 12:19:42 +0100

    The main class for the Jobinow application. This class contains the main method to start the Spring Boot application.

---
+++ backend/src/main/java/com/ouharri/Jobinow/Jobinow.java
@@ -0,0 +1,22 @@
+package com.ouharri.Jobinow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class for the Jobinow application.
+ * This class contains the main method to start the Spring Boot application.
+ */
+@SpringBootApplication
+public class Jobinow {
+
+    /**
+     * The main method to start the File Uploader application.
+     *
+     * @param args Command-line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Jobinow.class, args);
+    }
+
+}
