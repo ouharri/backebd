667c0dcc7aa169255fbe7af48e997be53a3a93b5
ouharri <ouharrioutman@gmail.com>
2023-12-29 12:23:09 +0100

    Integration tests for the jobinow application. These tests ensure that the application context loads successfully.

---
+++ backend/src/test/java/com/ouharri/JobinowTests.java
@@ -0,0 +1,21 @@
+package com.ouharri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Integration tests for the jobinow application.
+ * These tests ensure that the application context loads successfully.
+ */
+@SpringBootTest
+class JobinowTests {
+
+    /**
+     * Test to verify that the application context loads without errors.
+     */
+    @Test
+    void contextLoads() {
+        // The test is empty as it only checks if the context loads successfully.
+    }
+
+}
