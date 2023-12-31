f83af8d87dee0512796994b15accd13f474db3f7
ouharri <ouharrioutman@gmail.com>
2023-12-31 04:11:58 +0100

    Represents a profile entity in the system. This class includes information about a profile such as its title, description, and associated job offers.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/entities/Profil.java
@@ -0,0 +1,46 @@
+package com.ouharri.Jobinow.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a profile entity in the system.
+ * This class includes information about a profile such as its title, description, and associated job offers.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ * @version 1.0
+ */
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profil extends AbstractEntity {
+
+    /**
+     * The title of the profile.
+     */
+    @NotBlank(message = "Profile title cannot be blank")
+    private String title;
+
+    /**
+     * The description of the profile.
+     */
+    @NotBlank(message = "Profile description cannot be blank")
+    private String description;
+
+    /**
+     * The list of job offers associated with the profile.
+     */
+    @ManyToMany(
+            targetEntity = Offre.class,
+            mappedBy = "profils"
+    )
+    private List<Offre> offres = new ArrayList<>();
+}
