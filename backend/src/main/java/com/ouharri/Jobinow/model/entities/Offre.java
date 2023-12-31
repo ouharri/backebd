e5de8eb645b9a968ee3ad838af824096725c617e
ouharri <ouharrioutman@gmail.com>
2023-12-31 04:07:59 +0100

    Represents a job offer entity in the system. This class includes information about a job offer such as its title, description, location, degree, salary, status, associated company, recruiter, and tags.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/entities/Offre.java
@@ -0,0 +1,96 @@
+package com.ouharri.Jobinow.model.entities;
+
+import com.ouharri.Jobinow.model.enums.OffreStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a job offer entity in the system.
+ * This class includes information about a job offer such as its title, description, location, degree,
+ * salary, status, associated company, recruiter, and tags.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ * @version 1.0
+ */
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Offre extends AbstractEntity {
+
+    /**
+     * The title of the job offer.
+     */
+    @NotBlank(message = "Job title cannot be blank")
+    private String title;
+
+    /**
+     * The description of the job offer.
+     */
+    @NotBlank(message = "Job description cannot be blank")
+    private String description;
+
+    /**
+     * The location of the job offer.
+     */
+    @NotBlank(message = "Job location cannot be blank")
+    private String location;
+
+    /**
+     * The degree required for the job offer.
+     */
+    @NotBlank(message = "Degree requirements cannot be blank")
+    private String degree;
+
+    /**
+     * The salary offered for the job.
+     */
+    @PositiveOrZero(message = "Salary must be a non-negative value")
+    private Double salary;
+
+    /**
+     * The status of the job offer.
+     */
+    @NotNull(message = "Job status cannot be null")
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private OffreStatus status;
+
+    /**
+     * The company associated with the job offer.
+     */
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            targetEntity = Company.class
+    )
+    @NotNull(message = "Company cannot be null")
+    private Company company;
+
+    /**
+     * The recruiter associated with the job offer.
+     */
+    @OneToOne(
+            mappedBy = "offre",
+            targetEntity = User.class
+    )
+    private User recruiter;
+
+    /**
+     * The list of tags associated with the job offer.
+     */
+    @ManyToMany(
+            targetEntity = Tag.class,
+            mappedBy = "offres"
+    )
+    private List<Tag> tags = new ArrayList<>();
+}
