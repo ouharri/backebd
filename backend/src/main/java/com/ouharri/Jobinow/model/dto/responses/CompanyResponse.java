e60d7329fe8765c623b900a2207ca822997bb51e
ouharri <ouharrioutman@gmail.com>
2024-01-01 18:03:50 +0100

    DTO for representing a response related to the Company. This DTO includes information such as the company name, email, phone number, image URL, address, and common response fields.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/dto/responses/CompanyResponse.java
@@ -0,0 +1,42 @@
+package com.ouharri.Jobinow.model.dto.responses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.ouharri.Jobinow.model.dto.basic.AddressDto;
+import com.ouharri.Jobinow.model.entities.Company;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.URL;
+
+/**
+ * DTO for representing a response related to {@link Company}.
+ * This DTO includes information such as the company name, email, phone number, image URL, address, and common response fields.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ */
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CompanyResponse extends AbstractResponse {
+    @NotBlank(message = "Company name cannot be blank")
+    String name;
+
+    @Email(message = "Email address must be valid")
+    String email;
+
+    @Pattern(message = "Phone number must have exactly 10 digits", regexp = "\\d{10}")
+    String phoneNumber;
+
+    @URL(message = "Image URL must be valid")
+    String imageUrl;
+
+    AddressDto address;
+}
