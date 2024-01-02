c2ce1606ae19cfb60ab4cbbf670bc52a3f4a310d
ouharri <ouharrioutman@gmail.com>
2024-01-02 08:40:26 +0000

    Configuration class for OpenAPI documentation settings.

--- backend/src/main/java/com/ouharri/Jobinow/config/OpenApiConfig.java
+++ backend/src/main/java/com/ouharri/Jobinow/config/OpenApiConfig.java
@@ -33,7 +33,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                         description = "Local ENV",
-                        url = "http://localhost:8083"
+                        url = "http://localhost:8084"
                 ),
                 @Server(
                         description = "PROD ENV",
