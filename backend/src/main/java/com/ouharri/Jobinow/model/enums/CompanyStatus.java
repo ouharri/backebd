d8a9777f0269da3adb972ec14f4ef71d7803ea26
ouharri <ouharrioutman@gmail.com>
2024-01-02 08:31:34 +0000

    Enum representing the status of a company.

---
+++ backend/src/main/java/com/ouharri/Jobinow/model/enums/CompanyStatus.java
@@ -0,0 +1,26 @@
+package com.ouharri.Jobinow.model.enums;
+
+/**
+ * Enum representing the status of a company.
+ */
+public enum CompanyStatus {
+    /**
+     * The company has been approved.
+     */
+    APPROVED,
+
+    /**
+     * The company is pending approval.
+     */
+    PENDING,
+
+    /**
+     * The company has been rejected.
+     */
+    REJECTED,
+
+    /**
+     * The company is blacklisted.
+     */
+    BLACKLISTED
+}
